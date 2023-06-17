60036b0d7f48b027617ea7ef47742bd9f12a02e5
adamgrubbs <adam.grubbs@amfam.com>
2023-06-16 23:52:15 -0500

    feat: added plugins

--- config.lua
+++ config.lua
@@ -110,6 +110,45 @@ end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
+  {
+    "doctorfree/cheatsheet.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      local ctactions = require("cheatsheet.telescope.actions")
+      require("cheatsheet").setup({
+        bundled_cheetsheets = {
+          enabled = { "default", "lua", "markdown", "regex", "netrw", "unicode" },
+          disabled = { "nerd-fonts" },
+        },
+        bundled_plugin_cheatsheets = {
+          enabled = {
+            "auto-session",
+            "goto-preview",
+            "octo.nvim",
+            "telescope.nvim",
+            "vim-easy-align",
+            "vim-sandwich",
+          },
+          disabled = { "gitsigns" },
+        },
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ["<CR>"] = ctactions.select_or_fill_commandline,
+          ["<A-CR>"] = ctactions.select_or_execute,
+          ["<C-Y>"] = ctactions.copy_cheat_value,
+          ["<C-E>"] = ctactions.edit_user_cheatsheet,
+        },
+      })
+    end,
+  },
+  { "RishabhRD/popfix" },
+  { "RishabhRD/nvim-cheat.sh" },
+
   { "pearofducks/ansible-vim" },
   { "github/copilot.vim" },
   { "tpope/vim-speeddating" },
